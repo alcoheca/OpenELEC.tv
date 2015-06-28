f6daa4353963cb5ece7f5efc87fd6dbbd678fc57
CvH <namerp@googlemail.com>
2015-06-28 22:43:38 +0200

    update dvb-firmware to 1.3 - add fws from ubuntu nonfree

--- packages/linux-firmware/dvb-firmware/package.mk
+++ packages/linux-firmware/dvb-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="dvb-firmware"
-PKG_VERSION="CvH-1.2"
+PKG_VERSION="CvH-1.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
