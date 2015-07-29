273e20eb1c0dc695e5591ccba3d986785dc41b52
CvH <namerp@googlemail.com>
2015-07-29 08:55:37 +0200

    update CrazyCat to 29.07.2015
    - Experimental DVBSky T9580, T982, T980C; TT-budget CT-4500.
    - si2168: optional dvb-c mode.
    - fix proper operation of Input Steam ID (ISI) and Physical Layer Scrambling (PLS) in drivers for TBS 6983, TBS 6905 and TBS 6908
    - initial support for TBS 6910, TBS 6902, TBS 6901
    - improve compatibility with Linux kernels 3.19.x and 4.0

--- packages/linux-drivers/crazycat/package.mk
+++ packages/linux-drivers/crazycat/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="crazycat"
-PKG_VERSION="68a4ccb"
+PKG_VERSION="322dd7b"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
