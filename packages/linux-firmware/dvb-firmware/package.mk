b45c78c7c52bf886d5daa53f0441c7bb1d739c0c
CvH <namerp@googlemail.com>
2015-08-18 08:10:10 +0200

    revert to allow github merge

--- packages/linux-firmware/dvb-firmware/package.mk
+++ packages/linux-firmware/dvb-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="dvb-firmware"
-PKG_VERSION="CvH-1.5"
+PKG_VERSION="0.0.48"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
