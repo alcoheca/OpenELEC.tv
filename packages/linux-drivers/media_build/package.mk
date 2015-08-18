cb24fe3e900f02149c802fffa518e4f4449b7663
CvH <namerp@googlemail.com>
2015-08-18 08:24:04 +0200

    update media_build to 17.08.2015

--- packages/linux-drivers/media_build/package.mk
+++ packages/linux-drivers/media_build/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="media_build"
-PKG_VERSION="46eba69"
+PKG_VERSION="9d2c570"
 
 # choose "LATEST" or a date like "2014-12-01-e8bd888" for the driver package
 # chose from here http://linuxtv.org/downloads/drivers/
