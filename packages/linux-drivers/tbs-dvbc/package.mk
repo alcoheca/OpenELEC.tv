416b495a6be4a793fe534a0576314e11fcf8910c
CvH <namerp@googlemail.com>
2015-11-06 11:52:57 +0100

    update tbs to 05.11.2015

--- packages/linux-drivers/tbs-dvbc/package.mk
+++ packages/linux-drivers/tbs-dvbc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="tbs-dvbc"
-PKG_VERSION="151026"
+PKG_VERSION="151105"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
