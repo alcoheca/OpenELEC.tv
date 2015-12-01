323c328eb01f41d949c034f909c6ae9ed2e348ff
CvH <namerp@googlemail.com>
2015-12-01 21:57:28 +0100

    update CrazyCat to 01.12.2015

--- packages/linux-drivers/crazycat/package.mk
+++ packages/linux-drivers/crazycat/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="crazycat"
-PKG_VERSION="a306620"
+PKG_VERSION="f0685bd"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
