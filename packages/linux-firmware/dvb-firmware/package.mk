4612c4d006e84b549729df25a8dd696985067b9b
CvH <namerp@googlemail.com>
2016-01-08 19:23:19 +0100

    update dvb-firmware to 1.8
    - changes from upstream

--- packages/linux-firmware/dvb-firmware/package.mk
+++ packages/linux-firmware/dvb-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="dvb-firmware"
-PKG_VERSION="0.0.51"
+PKG_VERSION="CvH-1.8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
