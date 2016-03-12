a01e6c08961f616cc2452bfc2df3128039ed337e
CvH <namerp@googlemail.com>
2016-03-12 11:44:02 +0100

    update ljalves-cc to 16.03.2016

--- packages/linux-drivers/ljalves-cc/package.mk
+++ packages/linux-drivers/ljalves-cc/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ljalves-cc"
-PKG_VERSION="2016-01-30"
+PKG_VERSION="2016-03-11"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
